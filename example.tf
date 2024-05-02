3f93857bca4d8393528ce80a18a4f8b28af3c0b5
bot-run <bot-run@users.noreply.github.com>
2024-05-02 14:11:30 +0000

    [RUNTREQ-5555] : Onboard new user

--- example.tf
+++ example.tf
@@ -22,3 +22,14 @@ module "sumit_agrawal_partner_bmw_de" {
    ad_invite         = true
  }
 
+module "75dveen_drasah_gmail_com" {
+   source = "./modules/user"
+
+   email_address     = "75dveen.drasah@gmail.com"
+   first_name        = "75dveen"
+   last_name         = "Drasah"
+   bmw_department    = "DE-47"
+   bmw_employee_type = "gmail"
+   github_username   = "75dveenDrasah"
+   ad_invite         = false
+ }
